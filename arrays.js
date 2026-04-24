cac21f84cb863400d555e14d394410e184b4fa8e
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2026-04-24 08:19:31 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,14 @@
+const listaCelular =[
+    {nome: "Moto G54", preco:1200},
+    {nome: "Galaxy S23", preco:4000},
+    {nome: "Samsung A14", preco:900},
+    {nome: "Iphone 11", preco:2500},
+    {nome: "Iphone 13", preco:3500},
+    {nome: "Poco X5", preco:1300},
+    {nome: "Xiaomi Note 12", preco:1000},
+    {nome: "Realme 11", preco:1500},
+    {nome: "Asus ROG Phone", preco:5000},
+    {nome: "iphone 14", preco:4500},
+];
+
+module.exports = listaCelular;
